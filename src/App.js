16c3e0afa90ea28de059d198e23f58aa0182ca26
Aditi <adititrehan61@gmail.com>
2020-10-30 16:22:46 +0530

    added time param

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import {isTablet,osVersion,mobileVendor,mobileModel,deviceType,
   getUA,engineVersion,browserVersion,fullBrowserVersion,osName} from 'react-device-detect';
 import uniqid from 'uniqid';
 
+let is24Hour;
 function App() {
-  const hour = new Date().getHours()
-  const is24hour = hour > 12 ? true : false
+  var date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+    var dateString = date.toLocaleTimeString();
+
+   console.log(dateString,"dateString")
+    if (dateString.match(/am|pm/i) || date.toString().match(/am|pm/i) )
+    {
+        //12 hour clock
+       is24Hour = false
+    }
+    else
+    {
+        //24 hour clock
+       is24Hour = true
+    }
+    console.log(is24Hour)
   return (
     <div className="App">
       <header className="App-header">
@@ -23,7 +37,7 @@ function App() {
           <p>browserVersion- <code>{browserVersion}</code></p>
           <p>fullBrowserVersion- <code>{fullBrowserVersion}</code></p>
           <p>uniqueId-<code>{uniqid()}</code></p> 
-          <p>is24Hour-<code>{is24hour ? "true" : "false"}</code></p>
+          <p>is24Hour-<code>{is24Hour ? "true" : "false"}</code></p>
       </header>
     </div>
   );
