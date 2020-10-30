38d54c996e8a4120a89501be89fde1d620a0e485
Aditi <adititrehan61@gmail.com>
2020-10-30 13:33:14 +0530

    Added params

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {isTablet,osVersion,mobileVendor,mobileModel,deviceType,getUA,engineVersion,browserVersion,fullBrowserVersion} from 'react-device-detect';
+import {isTablet,osVersion,mobileVendor,mobileModel,deviceType,
+  getUA,engineVersion,browserVersion,fullBrowserVersion,osName} from 'react-device-detect';
 
 function App() {
   console.log(isTablet,osVersion,mobileVendor,mobileModel,deviceType,"isMobile")
@@ -9,15 +10,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-          <p>isTablet <code>{isTablet}</code></p> 
-          <p>osVersion <code>{osVersion}</code></p>
-          <p>mobileVendor <code>{mobileVendor}</code></p>
-          <p>mobileModel <code>{mobileModel}</code></p>
-          <p>deviceType <code>{deviceType}</code></p>
-          <p>getUA <code>{getUA}</code></p>
-          <p>engineVersion <code>{engineVersion}</code></p>
-          <p>browserVersion <code>{browserVersion}</code></p>
-          <p>fullBrowserVersion <code>{fullBrowserVersion}</code></p>
+          <p>isTablet- <code>{isTablet ? "true": "false"}</code></p> 
+          <p>osVersion- <code>{osVersion}</code></p>
+          <p>brand- <code>{mobileVendor}</code></p>
+          <p>deviceName- <code>{mobileModel}</code></p>
+          <p>deviceType- <code>{deviceType}</code></p>
+          <p>userAgent- <code>{getUA}</code></p>
+          <p>systemName- <code>{osName}</code></p>
+          <p>engineVersion- <code>{engineVersion}</code></p>
+          <p>browserVersion- <code>{browserVersion}</code></p>
+          <p>fullBrowserVersion- <code>{fullBrowserVersion}</code></p>
       </header>
     </div>
   );
